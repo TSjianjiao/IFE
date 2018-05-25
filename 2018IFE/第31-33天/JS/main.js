2c39abe1908b657af3ff74fea64caeb4a9d318e1
TSjianjiao <929175050@qq.com>
2018-05-25 12:43:00 +0800

    webpack打包后

--- 2018IFE/第31-33天/JS/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file 入口
- */
-const form = require('./form');
-var regionWrapper = document.getElementById('region-radio-wrapper');
-var productWrapper = document.getElementById('product-radio-wrapper');
-form.checkBoxEvent(regionWrapper);
-form.checkBoxEvent(productWrapper);
